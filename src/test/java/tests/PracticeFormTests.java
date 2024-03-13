8d69f9b29248da45a70f206f728d93ae29a79d19
Evgeniia Liasheva <evgenia_lozovik@mail.ru>
2024-03-14 00:59:02 +0300

    Fixes after review: xpath usage replaced with css selectors, date picker,upload picture, options and check boxes selections corrected, ads hided, sleep() deleted, selenide element replaced.

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,29 +1,41 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTests {
+    String userName = "Evgeniia";
+    String lastName = "Liasheva";
+    String email = "evgenia_lozovik@mail.ru";
+    String gender = "Female";
+    String mobile = "9110002233";
+    String year = "2020";
+    String month = "November";
+    String day = "18"; // format: 2 digits, example - "01"
+    String subject = "English";
+    String checkBoxNameSport = "Sports";
+    String checkBoxNameReading = "Reading";
+    String checkBoxNameMusic = "Music";
+    String picture = "ireland.jpg";
+    String address = "Some address, 1/5";
+    String state = "Haryana";
+    String city = "Karnal";
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        //Configuration.holdBrowserOpen = true; // only for local test purposes
+        //Configuration.timeout = 5000; // was changed for test purposes, default value is 4000
     }
 
     @AfterAll
@@ -33,60 +45,68 @@ public class PracticeFormTests {
 
     @Test
     void fillPracticeFormTest() {
-        String userName = "Evgeniia";
-        String lastName = "Liasheva";
-        String email = "evgenia_lozovik@mail.ru";
-        String gender = "Female";
-        String mobile = "9110002233";
-        LocalDate dateOfBirth = LocalDate.parse("2020-11-18");
-        String address = "Some address, 1/5";
-
         open("/automation-practice-form");
+        // to hide ads
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+
         $("#firstName").setValue(userName);
         $("#lastName").setValue(lastName);
-        $("#userEmail").setValue(email);
-        $(By.xpath("//input[@name='gender' and @value='"+gender+"']/..//label")).scrollTo().click();
+        $("#userEmail").setValue(email).click();
+
+        // just to keep the initial solution:
+        //$(By.xpath("//input[@name='gender' and @value='"+gender+"']/..//label")).scrollTo().click();
+
+        $("#genterWrapper").$(byText(gender)).click();
         $("#userNumber").setValue(mobile);
+
+        /* just to keep the initial solution:
         SelenideElement datePickerInput = $("#dateOfBirthInput");
         datePickerInput.sendKeys(Keys.CONTROL + "a");
-        datePickerInput.sendKeys(dateOfBirth.toString() + Keys.ENTER);
-        SelenideElement subjectPickerInput =$(By.cssSelector("#subjectsInput"));
-        subjectPickerInput.click();
-        sleep(500);
-        subjectPickerInput.sendKeys("Eng");
-        sleep(500);
-        subjectPickerInput.sendKeys(  Keys.ARROW_DOWN + "" + Keys.ENTER);
-        sleep(500);
-        $(By.xpath("//input[@id='hobbies-checkbox-1']/..//label")).scrollTo().click();
-        $(By.xpath("//input[@id='hobbies-checkbox-2']/..//label")).scrollTo().click();
-        $(By.xpath("//input[@id='hobbies-checkbox-3']/..//label")).scrollTo().click();
+        datePickerInput.sendKeys(dateOfBirth.toString() + Keys.ENTER); */
 
-        $("#uploadPicture").sendKeys("C:\\Users\\Evgeniia\\Desktop\\1.jpg");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__day--0" + day).click(); // day format: 2 digits, example: "01"
+
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText(checkBoxNameSport)).click();
+        $("#hobbiesWrapper").$(byText(checkBoxNameReading)).click();
+        $("#hobbiesWrapper").$(byText(checkBoxNameMusic)).click();
+
+        $("#uploadPicture").uploadFromClasspath(picture);
         $("#currentAddress").setValue(address);
-        $("#state").click();
-        sleep(500);
-        $("#state input").sendKeys(Keys.ENTER);
-        sleep(500);
-        $("#city").click();
-        sleep(500);
-        $("#city input").sendKeys(Keys.ENTER);
-        sleep(500);
+        $("div#state input").setValue(state).pressEnter();
+        $("div#city input").setValue(city).pressEnter();
         $("#submit").click();
 
+
+        // assert
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text(userName + " " + lastName));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text(email));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text(gender));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text(mobile));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text(day + " " + month + "," + year));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(subject));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text(subject));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text(checkBoxNameSport + ", " + checkBoxNameReading + ", " + checkBoxNameMusic));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text(picture));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text(address));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text(state + " " + city));
+
+        /* just to keep the initial solution:
         List<SelenideElement> tableRows = $$(By.xpath("//div[@role='dialog']//tbody/tr"));
         tableRows.get(0).$$("td").get(1).shouldHave(text(userName+" "+ lastName));
         tableRows.get(1).$$("td").get(1).shouldHave(text(email));
         tableRows.get(2).$$("td").get(1).shouldHave(text(gender));
         tableRows.get(3).$$("td").get(1).shouldHave(text(mobile));
-
-        String pattern = "dd MMMM,yyyy";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        tableRows.get(4).$$("td").get(1).shouldHave(text(formatter.format(dateOfBirth)));
-
-        tableRows.get(5).$$("td").get(1).shouldHave(text("English"));
-        tableRows.get(6).$$("td").get(1).shouldHave(text("Sports, Reading, Music"));
-        tableRows.get(7).$$("td").get(1).shouldHave(text("1.jpg"));
+        tableRows.get(4).$$("td").get(1).shouldHave(text(day+" "+month+","+year));
+        tableRows.get(5).$$("td").get(1).shouldHave(text(subject));
+        tableRows.get(6).$$("td").get(1).shouldHave(text(checkBoxNameSport+", "+checkBoxNameReading+", "+checkBoxNameMusic));
+        tableRows.get(7).$$("td").get(1).shouldHave(text(picture));
         tableRows.get(8).$$("td").get(1).shouldHave(text(address));
-        tableRows.get(9).$$("td").get(1).shouldHave(text("NCR Delhi"));
+        tableRows.get(9).$$("td").get(1).shouldHave(text(state + " " + city)); */
     }
 }
